62df2ddcb2b7e6a654a3a47dcd54de7aaa98d00b
idksarah <sarah.p.ngai@gmail.com>
2024-10-22 01:11:44 -0700

    help

--- shopping/index.js
+++ shopping/index.js
@@ -1,9 +1,24 @@
 var loadingMessage = document.querySelector('.loadingMessage');
+var messageOne = document.querySelector('.messageOne');
+var messageTwo = document.querySelector('.messageTwo');
+var messageTwoAndaHalf = document.querySelector('.messageTwoAndaHalf');
+var messageThree = document.querySelector('.messageThree');
+var messageFour = document.querySelector('.messageFour');
+var messageFive = document.querySelector('.messageFive');
+var messageSix = document.querySelector('.messageSix');
+var typingSpace = document.querySelector('.typingSpace');
 var linkOne = document.querySelector('.linkOne');
 var linkTwo = document.querySelector('.linkTwo');
 var linkThree = document.querySelector('.linkThree');
 var linkFour = document.querySelector('.linkFour');
 var links = [linkOne, linkTwo, linkThree, linkFour];
+function loadVariable() {
+    return localStorage.getItem('viewedOnce') === 'true';
+}
+function saveVariable() {
+    localStorage.setItem('viewedOnce', 'true');
+}
+var viewedOnce = loadVariable();
 function saveLinkState(index) {
     localStorage.setItem("link".concat(index, "Clicked"), 'true');
 }
@@ -35,6 +50,45 @@ links.forEach(function (link, index) {
     });
 });
 loadLinkState();
-setTimeout(function () {
+document.addEventListener('paste', function () {
+    messageFive.classList.remove('hidden');
+    setTimeout(function () {
+        messageSix.classList.remove('hidden');
+    }, 2000);
+    messageSix.id = 'link';
+    messageSix.addEventListener('click', function () {
+        window.location.href = './../aboutUs/index.html';
+    });
+});
+if (!viewedOnce) {
+    setTimeout(function () {
+        loadingMessage.classList.add('hidden');
+        var showMessages = function () {
+            messageOne === null || messageOne === void 0 ? void 0 : messageOne.classList.remove('hidden');
+            typingSpace === null || typingSpace === void 0 ? void 0 : typingSpace.classList.remove('hidden');
+            setTimeout(function () {
+                messageTwo === null || messageTwo === void 0 ? void 0 : messageTwo.classList.remove('hidden');
+                setTimeout(function () {
+                    messageTwoAndaHalf === null || messageTwoAndaHalf === void 0 ? void 0 : messageTwoAndaHalf.classList.remove('hidden');
+                    setTimeout(function () {
+                        messageThree === null || messageThree === void 0 ? void 0 : messageThree.classList.remove('hidden');
+                        setTimeout(function () {
+                            messageFour === null || messageFour === void 0 ? void 0 : messageFour.classList.remove('hidden');
+                            saveVariable();
+                        }, 2000);
+                    }, 2500);
+                }, 3000);
+            }, 1500);
+        };
+        setTimeout(showMessages, 1000);
+    }, 2000);
+}
+else {
     loadingMessage.classList.add('hidden');
-}, 2000);
+    messageOne === null || messageOne === void 0 ? void 0 : messageOne.classList.remove('hidden');
+    messageTwo === null || messageTwo === void 0 ? void 0 : messageTwo.classList.remove('hidden');
+    messageTwoAndaHalf === null || messageTwoAndaHalf === void 0 ? void 0 : messageTwoAndaHalf.classList.remove('hidden');
+    messageThree === null || messageThree === void 0 ? void 0 : messageThree.classList.remove('hidden');
+    messageFour === null || messageFour === void 0 ? void 0 : messageFour.classList.remove('hidden');
+    typingSpace === null || typingSpace === void 0 ? void 0 : typingSpace.classList.remove('hidden');
+}
--- shopping/index.ts
+++ shopping/index.ts
@@ -1,4 +1,12 @@
 const loadingMessage = document.querySelector<HTMLDivElement>('.loadingMessage')!;
+const messageOne = document.querySelector<HTMLDivElement>('.messageOne');
+const messageTwo = document.querySelector<HTMLDivElement>('.messageTwo');
+const messageTwoAndaHalf = document.querySelector<HTMLDivElement>('.messageTwoAndaHalf');
+const messageThree = document.querySelector<HTMLDivElement>('.messageThree');
+const messageFour = document.querySelector<HTMLDivElement>('.messageFour');
+const messageFive = document.querySelector<HTMLDivElement>('.messageFive')!;
+const messageSix = document.querySelector<HTMLDivElement>('.messageSix')!;
+const typingSpace = document.querySelector<HTMLDivElement>('.typingSpace');
 
 const linkOne = document.querySelector<HTMLSpanElement>('.linkOne');
 const linkTwo = document.querySelector<HTMLSpanElement>('.linkTwo');
@@ -6,6 +14,16 @@ const linkThree = document.querySelector<HTMLSpanElement>('.linkThree');
 const linkFour = document.querySelector<HTMLSpanElement>('.linkFour');
 
 const links = [linkOne, linkTwo, linkThree, linkFour];
+
+function loadVariable() {
+    return localStorage.getItem('viewedOnce') === 'true';
+}
+
+function saveVariable() {
+    localStorage.setItem('viewedOnce', 'true');
+}
+
+const viewedOnce = loadVariable();
 
 function saveLinkState(index: number) {
     localStorage.setItem(`link${index}Clicked`, 'true');
@@ -23,27 +41,67 @@ links.forEach((link, index) => {
     link?.addEventListener('click', () => {
         link.classList.add('clickedLink');
         saveLinkState(index + 1);
-        
-        switch (index+1) {
+
+        switch (index + 1) {
             case 1:
-            window.location.href = './bagLinks/linkOne.html';
-            break;
+                window.location.href = './bagLinks/linkOne.html';
+                break;
             case 2:
-            window.location.href = './bagLinks/linkTwo.html';
-            break;
+                window.location.href = './bagLinks/linkTwo.html';
+                break;
             case 3:
-            window.location.href = './bagLinks/linkThree.html';
-            break;
+                window.location.href = './bagLinks/linkThree.html';
+                break;
             case 4:
-            window.location.href = './bagLinks/linkFour.html';
-            break;
+                window.location.href = './bagLinks/linkFour.html';
+                break;
         }
     });
 });
 
 loadLinkState();
 
-setTimeout(() => {
-    loadingMessage.classList.add('hidden');
-}, 2000);
+document.addEventListener('paste', function () {
+    messageFive.classList.remove('hidden');
+    setTimeout(() => {
+        messageSix.classList.remove('hidden');
+    }, 2000);
+    messageSix.id = 'link';
+    messageSix.addEventListener('click', () => {
+        window.location.href = './../aboutUs/index.html';
+    });
+});
 
+if (!viewedOnce) {
+    setTimeout(() => {
+        loadingMessage.classList.add('hidden');
+
+        const showMessages = () => {
+            messageOne?.classList.remove('hidden');
+            typingSpace?.classList.remove('hidden');
+            setTimeout(() => {
+                messageTwo?.classList.remove('hidden');
+                setTimeout(() => {
+                    messageTwoAndaHalf?.classList.remove('hidden');
+                    setTimeout(() => {
+                        messageThree?.classList.remove('hidden');
+                        setTimeout(() => {
+                            messageFour?.classList.remove('hidden');
+                            saveVariable();
+                        }, 2000);
+                    }, 2500);
+                }, 3000);
+            }, 1500);
+        };
+
+        setTimeout(showMessages, 1000);
+    }, 2000);
+} else {
+    loadingMessage.classList.add('hidden');
+    messageOne?.classList.remove('hidden');
+    messageTwo?.classList.remove('hidden');
+    messageTwoAndaHalf?.classList.remove('hidden');
+    messageThree?.classList.remove('hidden');
+    messageFour?.classList.remove('hidden');
+    typingSpace?.classList.remove('hidden');
+}
